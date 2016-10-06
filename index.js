1d81efa7cc4a42d2d88a86f1aab73d7215c93085
DaveJ <dave@davejeffery.com>
2016-10-06 22:57:50 +0100

    Switch to promise interface and return native image

--- index.js
+++ index.js
@@ -4,40 +4,41 @@ var fs = require('fs');
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow || electron.remote.BrowserWindow;
 
-function screenshot(opt, cb) {
-  cb = cb || function() {};
-  if (opt.url && opt.height && opt.width && opt.filename) {
-     captureUrl(opt, cb);
+function screenshot(opt) {
+  if (opt.url && opt.height && opt.width) {
+     return captureUrl(opt);
   } else {
-    throw new Error('options `url`, `filename`, `width` and `height` are required');
+    Promise.reject('options `url`, `width` and `height` are required');
   }
 }
 
-function captureUrl(opt, cb) {
-  var win = new BrowserWindow({
-    x:0,
-    y:0,
-    width: opt.width,
-    height: opt.height,
-    show: false,
-    frame: false,
-    enableLargerThanScreen: true,
-    webPreferences: {
-      nodeIntegration: false
-    }
-  });
+function captureUrl(opt) {
+  return new Promise((resolve) => {
+    var win = new BrowserWindow({
+      x:0,
+      y:0,
+      width: opt.width,
+      height: opt.height,
+      show: false,
+      frame: false,
+      enableLargerThanScreen: true,
+      webPreferences: {
+        nodeIntegration: false
+      }
+    });
 
-  win.on('closed', function() {
-    win = null;
-  });
+    win.on('closed', function() {
+      win = null;
+    });
 
-  win.loadURL(opt.url);
-  win.webContents.on('did-finish-load', function() {
-    setTimeout(function() {
-      win.capturePage(function(img) {
-        fs.writeFile(opt.filename, img.toPng(), cb);
-        win.close();
-      });
-    }, (opt.delay || 0));
+    win.loadURL(opt.url);
+    win.webContents.on('did-finish-load', function() {
+      setTimeout(function() {
+        win.capturePage(function(img) {
+          resolve(img);
+          win.close();
+        });
+      }, (opt.delay || 0));
+    });
   });
 }
